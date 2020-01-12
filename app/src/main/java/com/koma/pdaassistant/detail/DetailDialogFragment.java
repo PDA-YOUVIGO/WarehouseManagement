bc61ce4290ee089a822767341e79921554212450
koma_mj <junmaokoma@gmail.com>
2020-01-12 12:11:31 +0800

    Add detail page layout and logic.

--- app/src/main/java/com/koma/pdaassistant/detail/DetailDialogFragment.java
+++ app/src/main/java/com/koma/pdaassistant/detail/DetailDialogFragment.java
@@ -16,23 +16,48 @@
 
 package com.koma.pdaassistant.detail;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.koma.pdaassistant.R;
 import com.koma.pdaassistant.data.entities.Shelving;
 
+import org.jetbrains.annotations.NotNull;
+
 public class DetailDialogFragment extends DialogFragment {
     private static final String TAG = "DetailDialogFragment";
     private static final String KEY_SHELVING = "key_shelving";
+
+    // 物料编码
+    private TextView materialCoding;
+    // 物料名称
+    private TextView materialName;
+    // 批次号
+    private TextView batchNumber;
+    // 规格
+    private TextView specification;
+    // 供应商批次
+    private TextView supplierBatch;
+    // 货位码
+    private TextView cargoCode;
+    // 未上架
+    private TextView notListedFirst, notListedSecond;
+    // 已上架
+    private TextView listedFirst, listedSecond;
+    // 当前上架
+    private EditText listingFirst, listingSecond, listingThird, listingFourth;
 
     private Context context;
 
@@ -69,16 +94,50 @@ public class DetailDialogFragment extends DialogFragment {
         }
     }
 
-    @Nullable
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.detail_dialog_fragment, container, false);
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(context).inflate(R.layout.detail_dialog_fragment, null);
+
+        initViews(view);
+
+        return buildDialog(view);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    @NotNull
+    private AlertDialog buildDialog(View view) {
+        return new AlertDialog.Builder(context)
+                .setTitle(R.string.detail)
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog1, int which) {
+                        // 点击取消时回调
+                    }
+                })
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog1, int which) {
+                        // 点击确定时回调
+                    }
+                })
+                .setView(view)
+                .create();
+    }
 
-
+    private void initViews(View view) {
+        materialCoding = view.findViewById(R.id.tv_material_coding_description);
+        materialName = view.findViewById(R.id.tv_material_name_description);
+        batchNumber = view.findViewById(R.id.tv_batch_number_description);
+        specification = view.findViewById(R.id.tv_specification_description);
+        supplierBatch = view.findViewById(R.id.tv_supplier_batch_description);
+        cargoCode = view.findViewById(R.id.tv_cargo_code_description);
+        notListedFirst = view.findViewById(R.id.tv_not_listed_description_first);
+        notListedSecond = view.findViewById(R.id.tv_not_listed_description_second);
+        listedFirst = view.findViewById(R.id.tv_listed_description_first);
+        listedSecond = view.findViewById(R.id.tv_listed_description_second);
+        listingFirst = view.findViewById(R.id.tv_listing_description_first);
+        listingSecond = view.findViewById(R.id.tv_listing_description_second);
+        listingThird = view.findViewById(R.id.tv_listing_description_third);
+        listingFourth = view.findViewById(R.id.tv_listing_description_fourth);
     }
 }
